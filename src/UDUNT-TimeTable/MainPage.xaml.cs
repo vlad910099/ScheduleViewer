2d2965c5c515eabf06001154b4a26f0fdae6b27b
vlad_zab <62466549+vlad910099@users.noreply.github.com>
2023-04-27 13:50:07 +0300

    11

---
+++ src/UDUNT-TimeTable/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UDUNT_TimeTable
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
